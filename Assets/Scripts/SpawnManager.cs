b6f70f7d78683675a4a40d1a56a73dd7115b9c28
Nischal Joshi <076bct051@ioepc.edu.np>
2022-05-12 18:28:30 +0545

    Updated Collision

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] spawnAnimal;
-    private float spawnRangeX=20f;
-    private float spawnPositionZ=20f;
+    private float spawnRangeX=15f;
+    private float spawnPositionZ=25f;
+    private float spawnDelay = 2f;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0,spawnAnimal.Length);
-            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
-            Instantiate(spawnAnimal[animalIndex],spawnPosition, spawnAnimal[animalIndex].transform.rotation);
-        }
+     
+    }
+    void SpawnRandomAnimal()
+	{
+        int animalIndex = Random.Range(0, spawnAnimal.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+        Instantiate(spawnAnimal[animalIndex], spawnPosition, spawnAnimal[animalIndex].transform.rotation);
     }
 }
