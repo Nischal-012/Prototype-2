1d6e0805e3c291c3b9cd3516d35a49ce7f77905d
Nischal Joshi <076bct051@ioepc.edu.np>
2022-05-12 11:20:54 +0545

    Prototype 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 20f;
+    private float xRange = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+		{
+            transform.position = new Vector3(-xRange,transform.position.y,transform.position.z);
+		}
+        if(transform.position.x > xRange)
+		{
+            transform.position = new Vector3(xRange,transform.position.y,transform.position.z); 
+		}
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+}
